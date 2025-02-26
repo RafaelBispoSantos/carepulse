8125998ec6274fe8dc4c6e80db50f5b1212e3f13
Rafael Bispo <rafael_bispo23@hotmail.com>
2025-02-25 21:37:13 -0300

    RegisterFomer create Date_picker and Skeleton

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -11,6 +11,10 @@ import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
 import { createUser } from "@/lib/actions/patient.actions";
 import { FormFieldType } from "./PatientForm";
+import { GenderOptions } from "@/constants";
+import { RadioGroupItem,RadioGroup } from "../ui/radio-group";
+import { Label } from "../ui/label";
+
 
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
@@ -46,10 +50,19 @@ const RegisterForm = ({ user }: { user: User }) => {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-          <h1 className="header">Hi there 👋🏽</h1>
-          <p className="text-dark-700">Schedule your first appointment.</p>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-12 flex-1"
+      >
+        <section className="space-y-4">
+          <h1 className="header">Welcome 👋🏽</h1>
+          <p className="text-dark-700">Let us know more about yourself</p>
+        </section>
+
+        <section className="space-y-4">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Personal Information</h2>
+          </div>
         </section>
         <CustomFornField
           fieldType={FormFieldType.INPUT}
@@ -60,6 +73,52 @@ const RegisterForm = ({ user }: { user: User }) => {
           iconSrc="/assets/icons/user.svg"
           iconAlt="user"
         />
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFornField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="Rafael@gmail.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+          />
+          <CustomFornField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="phone"
+            label="Phone number"
+            placeholder="(55)  1195882-9321"
+          />
+        </div>
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFornField
+            fieldType={FormFieldType.DATE_PICKER}
+            control={form.control}
+            name="birthDate"
+            label="Date of Birth"
+          />
+
+                <CustomFornField
+            fieldType={FormFieldType.SKELETON}
+            control={form.control}
+            name="gender"
+            label="Gender"
+            renderSkeleton={(field) => (
+              <RadioGroup className="flex h-11 gap-6 xl:justify-between" onValueChange={field.onChange} value={field.value}>
+                {GenderOptions.map((option) => (
+                  <div key={option}
+                  className="radio-group">
+                    <RadioGroupItem value={option} id={option}/>
+                    <Label htmlFor={option} className="cursor-pointer">{option}</Label>
+                  </div>
+                ))}
+              </RadioGroup>
+            )}
+          />
+        </div>
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
