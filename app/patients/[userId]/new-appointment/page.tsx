670ea85898ed18400debe658a15d139648d05d11
Rafael Bispo <rafael_bispo23@hotmail.com>
2025-02-28 13:55:10 -0300

    error params

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,10 +1,11 @@
 import AppointmentForm from "@/components/forms/AppointmentForm";
-import PatientForm from "@/components/forms/PatientForm";
-import { Button } from "@/components/ui/button";
 import { getPatient } from "@/lib/actions/patient.actions";
-
 import Image from "next/image";
 import Link from "next/link";
+
+interface SearchParamProps {
+  params: Record<string, string>; // Tipando corretamente os parâmetros
+}
 
 export default async function NewAppointment({ params }: SearchParamProps) {
   const { userId } = params;  // Desestruturando diretamente os parâmetros
@@ -12,7 +13,6 @@ export default async function NewAppointment({ params }: SearchParamProps) {
 
   return (
     <div className="flex h-screen max-h-screen">
-      
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[860px] flex-1 justify-between">
           <Image
@@ -23,22 +23,20 @@ export default async function NewAppointment({ params }: SearchParamProps) {
             className="mb-12 h-10 w-fit"
           />
           <AppointmentForm
-          type="create"
-          userId={userId}
-          patientId={patient?.$id}
+            type="create"
+            userId={userId}
+            patientId={patient?.$id}
           />
-         
-            <p className="copyright mt-10 py-12">&copy; 2025 CarePulse</p>
-            <Link href="/?admin=true" className="text-green-500"> Admin</Link>
-          
+          <p className="copyright mt-10 py-12">&copy; 2025 CarePulse</p>
+          <Link href="/?admin=true" className="text-green-500"> Admin</Link>
         </div>
       </section>
       <Image
-       src="/assets/images/appointment-img.png"
-       height={1000}
-       width={1000}
-       alt="appointment"
-       className="side-img max-w-[390px] bg-bottom"
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
       />
     </div>
   );
